e4b8f8865e592de977f00f152dd54d17b0e49e0d
Observer <kirillkuznetsov181@gmail.com>
2023-02-28 22:02:40 +0200

    add entity and pom

---
+++ src/main/java/com/service/todolist/dao/entities/Task.java
@@ -0,0 +1,26 @@
+package com.service.todolist.dao.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+}
